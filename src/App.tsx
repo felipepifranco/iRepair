fb5e12498e875472d51f4d7e9ba5c7d25b1e691b
felps <felipe.pifranco1@gmail.com>
2026-07-12 18:02:57 -0300

    fix(commits): corrige histórico de commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import { ServiceCard } from './components/ServiceCard'
 import NewServiceForm from './components/NewServiceForm'
 import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='flex flex-col gap-4'>
